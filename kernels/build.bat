47b20dfcf376a93b66ee749c039e527d7f28f5b0
Paul Robson <paul@robsons.org.uk>
2018-12-29 18:06:01 +0000

    update

--- kernels/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python ..\scripts\makecore.py core graphics.layer2
-..\bin\snasm __core.asm boot.img
-
-
--- kernels/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call build.bat
-..\bin\CSpect.exe -zxnext -brk -exit -esc -w3 ..\files\bootloader.sna
